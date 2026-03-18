7f33216d9118f961fcb2ff4ee5e981cd275daf18
Sarang Gujarathi <saranggujarathi79@gmail.com>
2026-03-18 20:45:53 +0530

    Add files via upload

---
+++ Assignment_Variables.R
@@ -0,0 +1,54 @@
+#The variable swap 
+
+a <- 10
+b <- 20
+temp <- a
+a <- b
+b <- a
+
+#The Equality Checker 
+secret_number <- 7
+secret_number ==10
+
+#Calculating a Total 
+apple_price <-  1.50 
+banana_price <- 0.75
+quantity <- 4
+
+total_cost <- (apple_price + banana_price) * quantity
+total_cost
+
+
+# String Comparison 
+
+password_stored <- "Python123"
+
+password_entered <- "python123"
+
+password_stored == password_entered
+
+#it giving false because in the first string P is capital and second string P is small.
+
+#The Boolean Assignment 
+
+age <- 15
+can_drive <- age >= 16
+can_drive
+
+#6. The "Update" Shortcut
+
+score <- 50
+score <- score + 15
+score
+
+#Comparing Expressions 
+10 ** 2 == 50 * 2
+
+#The Type Trap 
+s <- as.integer(5)
+typeof(s)
+y <- "6"
+typeof(y)
+s == y
+
+#Multi-Variable Assignment 
